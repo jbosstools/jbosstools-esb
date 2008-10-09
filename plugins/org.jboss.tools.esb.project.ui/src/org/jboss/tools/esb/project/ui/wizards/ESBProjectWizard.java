87a0860477d3cbbd87e56f5a30bffda68079ad32
Denny Xu <dxu@redhat.com>
2008-10-09 10:27:52 +0000

    JBIDE-2876: implement the feature
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10763 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.esb.project.ui/src/org/jboss/tools/esb/project/ui/wizards/ESBProjectWizard.java
+++ plugins/org.jboss.tools.esb.project.ui/src/org/jboss/tools/esb/project/ui/wizards/ESBProjectWizard.java
@@ -1,8 +1,19 @@
 package org.jboss.tools.esb.project.ui.wizards;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IFacetedProjectTemplate;
@@ -10,6 +21,8 @@ import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 import org.eclipse.wst.web.ui.internal.wizards.NewProjectDataModelFacetWizard;
 import org.jboss.tools.common.model.ui.ModelUIImages;
 import org.jboss.tools.esb.core.ESBProjectConstant;
+import org.jboss.tools.esb.core.ESBProjectCorePlugin;
+import org.jboss.tools.esb.core.facet.IJBossESBFacetDataModelProperties;
 import org.jboss.tools.esb.project.ui.messages.JBossESBUIMessages;
 import org.jboss.tools.esb.project.ui.wizards.pages.ESBProjectFirstPage;
 
@@ -46,6 +59,26 @@ public class ESBProjectWizard extends NewProjectDataModelFacetWizard implements
 	@Override
 	protected IFacetedProjectTemplate getTemplate() {
 		return ProjectFacetsManager.getTemplate(ESBProjectConstant.ESB_PROJECT_FACET_TEMPLATE);
+	}
+
+	@Override
+	protected void postPerformFinish() throws InvocationTargetException {
+		super.postPerformFinish();
+		String prjName = this.getProjectName();
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(prjName);
+		if(!project.exists()) return;
+		
+		try {
+			String esbcontent = project.getPersistentProperty(IJBossESBFacetDataModelProperties.QNAME_ESB_CONTENT_FOLDER);
+			IPath esbPath = new Path(esbcontent).append(ESBProjectConstant.META_INF);
+			IFile esbFile = project.getFolder(esbPath).getFile(ESBProjectConstant.ESB_CONFIG_JBOSSESB);
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			IDE.openEditor(page, esbFile);
+			
+		} catch (CoreException e) {
+			ESBProjectCorePlugin.getDefault().getLog().log(e.getStatus());
+		}
+		
 	}
 	
 	
