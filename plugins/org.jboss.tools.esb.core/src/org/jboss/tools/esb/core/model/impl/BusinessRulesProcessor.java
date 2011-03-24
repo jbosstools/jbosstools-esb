44f4cdd1ff5658f31344472fb798ba50f1f43f9f
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-03-24 23:48:04 +0000

    JBIDE-8466 https://issues.jboss.org/browse/JBIDE-8466
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.2.x@29997 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.esb.core/src/org/jboss/tools/esb/core/model/impl/BusinessRulesProcessor.java
+++ plugins/org.jboss.tools.esb.core/src/org/jboss/tools/esb/core/model/impl/BusinessRulesProcessor.java
@@ -1,49 +1,16 @@
 package org.jboss.tools.esb.core.model.impl;
 
-import org.jboss.tools.esb.core.model.ESBCustomizedObjectImpl;
+/**
+ * It seems that all ESB action classes extending ContentBasedRouter use the same set of attributes.
+ * It means that we may not need specific implementations for the inherited actions.
+ *  
+ * @author slava
+ *
+ */
 
-public class BusinessRulesProcessor extends ESBCustomizedObjectImpl {
+public class BusinessRulesProcessor extends ContentBasedRouter {
 	private static final long serialVersionUID = 1L;
-	public static String ATTR_RULE_AGENT_PROPERTIES = "rule agent properties";
-	public static String ATTR_DECISION_TABLE = "decision table";
-	public static String ATTR_RULE_MAX_THREADS = "rule max threads";
-	public static String ATTR_RULE_MULTITHREAD_EVALUATION = "rule multithread evaluation";
-
-	static int RULE_SET_BASED = 0;
-	static int RULE_AGENT_BASED = 1;
-	static int DECISION_TABLE_BASED = 2;
 
 	public BusinessRulesProcessor() {}
 
-	@Override
-	public boolean isAttributeEditable(String name) {
-		if(ContentBasedRouter.ATTR_RULE_SET.equals(name)) {
-			int kind = getKind();
-			return kind < 0 || kind == RULE_SET_BASED;			
-		} else if(ATTR_RULE_AGENT_PROPERTIES.equals(name)) {
-			int kind = getKind();
-			return kind < 0 || kind == RULE_AGENT_BASED;			
-		} else if(ATTR_DECISION_TABLE.equals(name)) {
-			int kind = getKind();
-			return kind < 0 || kind == DECISION_TABLE_BASED;			
-		}
-		if(ATTR_RULE_MAX_THREADS.equals(name)) {
-			String a = getAttributeValue(ATTR_RULE_MULTITHREAD_EVALUATION);
-			if("true".equals(a)) return true;
-			String b = getAttributeValue(ATTR_RULE_MAX_THREADS);
-			return b != null && b.length() > 0;
-			
-		}
-		return super.isAttributeEditable(name);
-	}
-	
-	int getKind() {
-		String ruleSet = getAttributeValue(ContentBasedRouter.ATTR_RULE_SET);
-		if(ruleSet != null && ruleSet.length() > 0) return RULE_SET_BASED;
-		String ruleAgent = getAttributeValue(ATTR_RULE_AGENT_PROPERTIES);
-		if(ruleAgent != null && ruleAgent.length() > 0) return RULE_AGENT_BASED;
-		String decisionTable = getAttributeValue(ATTR_DECISION_TABLE);
-		if(decisionTable != null && decisionTable.length() > 0) return DECISION_TABLE_BASED;
-		return -1;
-	}
 }
--- plugins/org.jboss.tools.esb.core/src/org/jboss/tools/esb/core/model/impl/ContentBasedRouter.java
+++ plugins/org.jboss.tools.esb.core/src/org/jboss/tools/esb/core/model/impl/ContentBasedRouter.java
@@ -23,6 +23,15 @@ public class ContentBasedRouter extends ESBCustomizedObjectImpl {
 	public static String ATTR_RULE_SET = "rule set";
 	public static String ATTR_RULE_RELOAD = "rule reload";
 
+	public static String ATTR_RULE_AGENT_PROPERTIES = "rule agent properties";
+	public static String ATTR_DECISION_TABLE = "decision table";
+	public static String ATTR_RULE_MAX_THREADS = "rule max threads";
+	public static String ATTR_RULE_MULTITHREAD_EVALUATION = "rule multithread evaluation";
+
+	static int RULE_SET_BASED = 0;
+	static int RULE_AGENT_BASED = 1;
+	static int DECISION_TABLE_BASED = 2;
+
 	public ContentBasedRouter() {}
 
 	@Override
@@ -38,6 +47,25 @@ public class ContentBasedRouter extends ESBCustomizedObjectImpl {
 				return false;
 			}
 		}
+	
+		if(ContentBasedRouter.ATTR_RULE_SET.equals(name)) {
+			int kind = getKind();
+			return kind < 0 || kind == RULE_SET_BASED;			
+		} else if(ATTR_RULE_AGENT_PROPERTIES.equals(name)) {
+			int kind = getKind();
+			return kind < 0 || kind == RULE_AGENT_BASED;			
+		} else if(ATTR_DECISION_TABLE.equals(name)) {
+			int kind = getKind();
+			return kind < 0 || kind == DECISION_TABLE_BASED;			
+		}
+		if(ATTR_RULE_MAX_THREADS.equals(name)) {
+			String a = getAttributeValue(ATTR_RULE_MULTITHREAD_EVALUATION);
+			if("true".equals(a)) return true;
+			String b = getAttributeValue(ATTR_RULE_MAX_THREADS);
+			return b != null && b.length() > 0;
+			
+		}
+
 		return super.isAttributeEditable(name);
 	}
 
@@ -59,5 +87,14 @@ public class ContentBasedRouter extends ESBCustomizedObjectImpl {
 		}
 	}
 
+	int getKind() {
+		String ruleSet = getAttributeValue(ContentBasedRouter.ATTR_RULE_SET);
+		if(ruleSet != null && ruleSet.length() > 0) return RULE_SET_BASED;
+		String ruleAgent = getAttributeValue(ATTR_RULE_AGENT_PROPERTIES);
+		if(ruleAgent != null && ruleAgent.length() > 0) return RULE_AGENT_BASED;
+		String decisionTable = getAttributeValue(ATTR_DECISION_TABLE);
+		if(decisionTable != null && decisionTable.length() > 0) return DECISION_TABLE_BASED;
+		return -1;
+	}
 	
 }
